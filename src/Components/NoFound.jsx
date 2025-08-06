6a4f888c180561ba79b73d7d233592cc8e8880e1
Muhammad tanveer <muhammadtanveertaqi@gmail.com>
2025-08-06 13:49:57 +0500

    first commit

--- src/Components/NoFound.jsx
+++ src/Components/NoFound.jsx
@@ -4,7 +4,7 @@ import { faCircleUser } from '@fortawesome/free-solid-svg-icons'
 
 function NoFound() {
   return (
-    <div className=' overflow-hidden flex flex-col items-center justify-center h-[400px] gap-4'>
+    <div className=' overflow-hidden flex flex-col items-center justify-center h-[400px] gap-4 text-white'>
         <FontAwesomeIcon icon={faCircleUser} className=' text-yellow-500 text-9xl' />
       <div>No Contact</div>
     </div>
